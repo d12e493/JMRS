22da02dbe0f7a31da7200655f757417d21ddad05
Davis Chen <d12e493@gmail.com>
2016-11-19 02:08:41 +0800

    拿掉用不到的頁面及 entity repository

--- src/main/java/idv/jmrs/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package idv.jmrs.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import idv.jmrs.entity.Role;
-
-public interface RoleRepository extends CrudRepository<Role, String> {
-
-}
